a7898131254fdc164fac5e497db925a101ee244f
allanore <orellanaluke@gmail.com>
2020-12-15 09:20:32 -0500

    Front-end

--- CloudSkillsChat/main.go
+++ CloudSkillsChat/main.go
@@ -4,57 +4,94 @@ import (
 	"net/http"
 	"html/template"
 	"fmt"
-	"log"
+
 )
 
 var tpl *template.Template
 
 func init() {
-	tpl = template.Must(template.ParseFiles("index.html"))
+
+	tpl = template.Must(template.ParseGlob("templates/*.html"))
 }
 
-
+type Comment struct {
+	ID int
+	Name   string
+	Message  string
+}
 func main() {
 	http.HandleFunc("/", index)
 	http.Handle("/css/", http.StripPrefix("/css", http.FileServer(http.Dir("./css"))))
+	http.HandleFunc("/message", messageCreateProcess)
+	http.HandleFunc("/update", updateChat)
+	http.HandleFunc("/chat", chatPage)
 	http.ListenAndServe(":8080", nil)
 }
 
 func index(res http.ResponseWriter, req *http.Request) {
 
-	err := req.ParseForm()
-	if err != nil {
-		log.Println(err)
+	if req.Method != "GET" {
+		http.Error(res, http.StatusText(405), http.StatusMethodNotAllowed)
+		return
 	}
-	fmt.Println(req.Form)
-	fmt.Printf("%T\n", req.Form)
 
-	data := struct{
-		Method string
-		URL string
-		Submissions map[string][]string
-	}{
-		req.Method,
-		req.URL.Path,
-		req.Form,
+
+	tpl.ExecuteTemplate(res, "index.html", nil)
+
+}
+
+func chatPage(res http.ResponseWriter, req *http.Request) {
+
+	if req.Method != "GET" {
+		http.Error(res, http.StatusText(405), http.StatusMethodNotAllowed)
+		return
 	}
-	tpl.ExecuteTemplate(res, "index.html", data)
+
+
+	tpl.ExecuteTemplate(res, "chat.html", nil)
+
 }
 
 
+func messageCreateProcess(res http.ResponseWriter, req *http.Request) {
+	if req.Method != "POST" {
+		http.Error(res, http.StatusText(405), http.StatusMethodNotAllowed)
+		return
+	}
+
+	// get form values
+	cmt := Comment{}
+	cmt.Name = req.FormValue("name")
+	cmt.Message = req.FormValue("message")
+
+	// validate form values
+	if cmt.Name == "" || cmt.Message == "" {
+		http.Error(res, http.StatusText(400), http.StatusBadRequest)
+		return
+	}
+
+	// INSERT SQL PORTION TO CREATE NEW TABLE RECORDS
 
 
 
 
+	// Redirect
+	http.Redirect(res, req, "/chat", http.StatusSeeOther)
+}
 
+func updateChat(res http.ResponseWriter, req *http.Request) {
 
+	if req.Method != "GET" {
+		http.Error(res, http.StatusText(405), http.StatusMethodNotAllowed)
+		return
+	}
 
+	// INSERT SQL QUERY TO GET THE ENTIRE CHAT LOG AND OUTPUT IT TO JSON
+	
 
+	//Pass JSON Chat log to page
+	bs := "Should be Chat log"
+	fmt.Fprint(res, string(bs))
 
-
-
-
-
-
-
+}
 
